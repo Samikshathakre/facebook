b6f478b4ca499ea0b4abb074dc8dd5ed5c85c531
samiksha <samikshathakre1113@gmail.com>
2022-12-16 23:09:18 +0530

    1st commit

---
+++ app/src/main/java/com/fb/facebook/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fb.facebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
